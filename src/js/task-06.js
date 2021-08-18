6505316cf0a73c638e937c5176a370adcc24b439
samuel shuman <81707204+kucherjavij@users.noreply.github.com>
2021-08-18 19:10:02 +0300

    sum89

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,10 +4,14 @@ console.log(dataLength);
 function validator(event) {
     if (event.currentTarget.value.length === dataLength) {
         event.currentTarget.classList.add("valid");
+        event.currentTarget.classList.remove("invalid");
+
         return;
     }
     if (event.currentTarget.value.length !== dataLength) {
         event.currentTarget.classList.add("invalid");
+        event.currentTarget.classList.remove("valid");
+
         return;
 
     }
